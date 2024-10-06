5724353a551ff5115f44d2907851e0cc0e5b4063
motolies <knw1234@gmail.com>
2024-10-06 22:20:54 +0900

    h2 환경 설정

--- src/test/java/kr/hvy/blog/BlogApplicationTests.java
+++ src/test/java/kr/hvy/blog/BlogApplicationTests.java
@@ -2,12 +2,14 @@ package kr.hvy.blog;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")  // 'application-test.yml' 사용
 class BlogApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Test
+  void contextLoads() {
+  }
 
 }

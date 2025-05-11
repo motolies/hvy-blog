29f19cbf35e691827435e368de6fdac8382ea3f4
Namwon Kim <knw1234@gmail.com>
2025-05-11 14:56:23 +0900

    파일 접근 권한 체크를 위한 FileAuthoritySpecification 클래스 추가 및 관련 서비스에서 권한 검증 로직 통합

--- src/main/java/kr/hvy/blog/modules/file/application/service/FileManagementService.java
+++ src/main/java/kr/hvy/blog/modules/file/application/service/FileManagementService.java
@@ -8,6 +8,7 @@ import kr.hvy.blog.modules.file.domain.FileMapper;
 import kr.hvy.blog.modules.file.domain.FileService;
 import kr.hvy.blog.modules.file.domain.dto.FileCreate;
 import kr.hvy.blog.modules.file.domain.dto.FileResponse;
+import kr.hvy.blog.modules.file.domain.specification.FileAuthoritySpecification;
 import kr.hvy.blog.modules.file.domain.specification.FileCreateSpecification;
 import kr.hvy.blog.modules.post.application.port.out.PostManagementPort;
 import kr.hvy.blog.modules.post.domain.Post;
@@ -26,6 +27,7 @@ public class FileManagementService extends AbstractFileManagementService impleme
   private final FileService fileService;
   private final PostManagementPort postManagementPort;
   private final FileCreateSpecification fileCreateSpecification = new FileCreateSpecification();
+  private final FileAuthoritySpecification fileAuthoritySpecification = new FileAuthoritySpecification();
   private final FileMapper fileMapper;
 
   public FileManagementService(@Value("${path.upload}") String rootLocation, FileManagementPort fileManagementPort, FileService fileService, PostManagementPort postManagementPort,
@@ -63,6 +65,10 @@ public class FileManagementService extends AbstractFileManagementService impleme
 
   @Override
   public DeleteResponse<Long> delete(Long id) {
+    // 파일 삭제 시 권한 체크
+    File file = fileManagementPort.findById(id);
+    fileAuthoritySpecification.validateException(file);
+
     fileManagementPort.deleteById(id);
     return DeleteResponse.<Long>builder()
         .id(id).build();
@@ -70,6 +76,8 @@ public class FileManagementService extends AbstractFileManagementService impleme
 
   @Override
   public List<FileResponse> findByPostId(Long postId) {
+    // 파일 목록 조회 시 권한 체크는 각 게시물에 대해 이미 수행됨
+    // 여기서는 게시물에 접근 가능한 경우에만 호출되므로 추가 체크 불필요
     return fileManagementPort.findByPostId(postId).stream()
         .map(fileMapper::toResponse)
         .toList();
--- src/main/java/kr/hvy/blog/modules/file/application/service/FilePublicService.java
+++ src/main/java/kr/hvy/blog/modules/file/application/service/FilePublicService.java
@@ -4,29 +4,31 @@ import kr.hvy.blog.modules.file.application.port.in.FilePublicUseCase;
 import kr.hvy.blog.modules.file.application.port.out.FileManagementPort;
 import kr.hvy.blog.modules.file.domain.File;
 import kr.hvy.blog.modules.file.domain.dto.FileResourceResponse;
+import kr.hvy.blog.modules.file.domain.specification.FileAuthoritySpecification;
 import kr.hvy.common.layer.UseCase;
 import org.springframework.beans.factory.annotation.Value;
 
 @UseCase
-
 public class FilePublicService extends AbstractFileManagementService implements FilePublicUseCase {
 
   private final FileManagementPort fileManagementPort;
+  private final FileAuthoritySpecification fileAuthoritySpecification = new FileAuthoritySpecification();
 
   public FilePublicService(@Value("${path.upload}") String rootLocation, FileManagementPort fileManagementPort) {
     super(rootLocation);
     this.fileManagementPort = fileManagementPort;
   }
 
-
   @Override
   public FileResourceResponse download(Long id) throws Exception {
     File file = fileManagementPort.findById(id);
+    // 권한 체크
+    fileAuthoritySpecification.validateException(file);
+
     return FileResourceResponse.builder()
         .originalName(file.getOriginName())
         .type(file.getType())
         .resource(loadAsResource(file.getPath()))
         .build();
   }
-
 }

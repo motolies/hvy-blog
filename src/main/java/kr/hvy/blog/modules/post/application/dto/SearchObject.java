b4492cf642d00c5bd2e699ef8ded3f7ee27fd119
Namwon Kim <knw1234@gmail.com>
2026-02-23 00:17:45 +0900

    페이징 수정

--- src/main/java/kr/hvy/blog/modules/post/application/dto/SearchObject.java
+++ src/main/java/kr/hvy/blog/modules/post/application/dto/SearchObject.java
@@ -3,13 +3,13 @@ package kr.hvy.blog.modules.post.application.dto;
 import java.util.List;
 import kr.hvy.blog.modules.post.domain.code.SearchType;
 import kr.hvy.common.application.domain.dto.paging.PageRequest;
-import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Value;
+import lombok.experimental.SuperBuilder;
 import lombok.extern.jackson.Jacksonized;
 
 @Value
-@Builder
+@SuperBuilder(toBuilder = true)
 @Jacksonized
 @EqualsAndHashCode(callSuper = true)
 public class SearchObject extends PageRequest {

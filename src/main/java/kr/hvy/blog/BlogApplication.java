ac5d853155e10e1e12fd639f176b68d795f1a6a1
Namwon Kim <knw1234@gmail.com>
2026-09-13 14:04:00 +0900

    모델 추가

--- src/main/java/kr/hvy/blog/BlogApplication.java
+++ src/main/java/kr/hvy/blog/BlogApplication.java
@@ -1,5 +1,6 @@
 package kr.hvy.blog;
 
+import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,16 +9,18 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+@Slf4j
 @SpringBootApplication
 @MapperScan(basePackages = {"kr.hvy.blog.modules.*.repository.mapper"})
 @EnableJpaRepositories(basePackages = {"kr.hvy.blog", "kr.hvy.common.aop.logging"})
 @EntityScan(basePackages = {"kr.hvy.blog", "kr.hvy.common.aop.logging"})
-//@ComponentScan(basePackages = {"kr.hvy.blog", "kr.hvy.common.infrastructure.redis", "kr.hvy.common.infrastructure.scheduler", "kr.hvy.common.infrastructure.notification"})
 @EnableAsync
 @EnableScheduling
 public class BlogApplication {
 
   public static void main(String[] args) {
+    log.info("Java Version : {}", System.getProperty("java.version"));
+    log.info("Java Home    : {}", System.getProperty("java.home"));
     SpringApplication.run(BlogApplication.class, args);
   }
 
